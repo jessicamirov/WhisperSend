62e51a84aea8dc5471fac7a4c9372a64c2eb91bf
jessicamirov <jessicamirov@gmail.com>
2024-08-25 13:41:51 +0300

    index.html update1

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
-import {render } from 'preact';
-import { App } from "./app.jsx";
-import './index.css';
+import { render } from "preact"
+import { App } from "./app.jsx" // ייבוא של הקומפוננטה הראשית
+import "./index.css" // ייבוא של קובץ ה-CSS
 
-render(<App />, document.getElementById('app'));
+// רנדרינג של הקומפוננטה הראשית לתוך האלמנט עם id="app"
+render(<App />, document.getElementById("app"))
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from "vite"
+import preact from "@preact/preset-vite"
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
-  build: {
-    rollupOptions: {
-      external: ['buffer', 'tweetnacl', 'elliptic'],
+    plugins: [preact()],
+    build: {
+        rollupOptions: {
+            external: ["buffer", "tweetnacl", "elliptic"],
+        },
     },
-  },
-  resolve: {
-    alias: {
-      buffer: 'buffer',
+    resolve: {
+        alias: {
+            buffer: "buffer",
+        },
     },
-  },
+    base: "./", // הגדרה של base כדי לוודא שהקבצים ייטענו נכון גם בתיקיות יחסיות
 })
