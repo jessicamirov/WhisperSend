a5d0937ac5dfd3148aafba7bc4413062e7c83767
jessicamirov <99083980+jessicamirov@users.noreply.github.com>
2024-08-25 13:00:48 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  build: {
+    rollupOptions: {
+      external: ['buffer', 'tweetnacl'],
+    },
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer', // תוודא שהמודול buffer יופנה בצורה נכונה
+    },
+  },
 })
