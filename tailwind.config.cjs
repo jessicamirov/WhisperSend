d1192fd3b21acb032e596e50818af311e9455c4e
jessicamirov <jessicamirov@gmail.com>
2024-09-20 23:33:55 +0300

    tailwind dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,17 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  theme: {
-    extend: {
-      colors: {
-        blue: {
-          900: '#1e3a8a',
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    theme: {
+        extend: {
+            colors: {
+                blue: {
+                    900: "#1e3a8a",
+                },
+                gray: {
+                    50: "#f9fafb",
+                },
+            },
         },
-        gray: {
-          50: '#f9fafb',
-        },
-      },
     },
-  },
-  plugins: [],
+    darkMode: "class",
+    plugins: [],
 }
